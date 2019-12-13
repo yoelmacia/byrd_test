9f29151682dee7b0395292d8d1e566c9875decf2
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2019-12-13 13:22:45 +0100

    Install the datepicker component
    Add the datepicker component to make the handling of dates easier,
    add v-model to communicate the component with a variable with
    the selected date. Add styles to align well the three elements.

--- src/components/Costumers.vue
+++ src/components/Costumers.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="valid" v-if="costumers.length">
-      <div>
+      <div class="valid-item">
         <span>id </span>
         <select v-model="selected">
           <option v-for="(item, index) in costumers" :key="index">
@@ -9,6 +9,16 @@
           </option>
         </select>
       </div>
+      <Datepicker
+        class="valid-item"
+        placeholder="Select Date From"
+        v-model="selectDateFrom"
+      ></Datepicker>
+      <Datepicker
+        class="valid-item"
+        placeholder="Select Date To"
+        v-model="selectDateTo"
+      ></Datepicker>
     </div>
     <div class="invalid" v-else>
       No data
@@ -18,12 +28,18 @@
 
 <script>
 import listAllCostumers from "../services/listAllCostumers";
+import Datepicker from "vuejs-datepicker";
 export default {
   name: "Costumers",
+  components: {
+    Datepicker
+  },
   data() {
     return {
       costumers: [],
-      selected: ""
+      selected: "",
+      selectDateFrom: "",
+      selectDateTo: ""
     };
   },
   created() {
@@ -42,4 +58,16 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.valid {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 100px;
+  background-color: aliceblue;
+}
+.valid-item {
+  margin: 10px auto;
+}
+</style>
