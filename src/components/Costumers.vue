e99a13ee298a311438972cd734cb40cceff87b73
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2019-12-20 18:43:02 +0100

    Formatting and change the button color

--- src/components/Costumers.vue
+++ src/components/Costumers.vue
@@ -1,72 +1,96 @@
 <template>
   <div>
     <div v-if="costumers.length">
-      <div class="valid">
-        <div class="valid-item">
-          <span>id </span>
-          <select v-model="selected">
-            <option v-for="(item, index) in costumers" :key="index">
-              {{ item.id }}
-            </option>
-          </select>
+      <form @submit.prevent="submit">
+        <div class="valid">
+          <div class="valid-item">
+            <span>id</span>
+            <select v-model="selected">
+              <option v-for="(item, index) in costumers" :key="index">{{
+                item.id
+              }}</option>
+            </select>
+          </div>
+          <Datepicker
+            class="valid-item"
+            placeholder="Select Date From"
+            :value="selectDateFrom"
+            format="yyyy-MM-dd"
+            @input="selectDateFrom = fixDate($event)"
+          ></Datepicker>
+          <Datepicker
+            class="valid-item"
+            placeholder="Select Date To"
+            :value="selectDateTo"
+            format="yyyy-MM-dd"
+            @input="selectDateTo = fixDate($event)"
+          ></Datepicker>
         </div>
-        <Datepicker
-          class="valid-item"
-          placeholder="Select Date From"
-          :value="selectDateFrom"
-          format="yyyy-MM-dd"
-          @input="selectDateFrom = fixDate($event)"
-        ></Datepicker>
-        <Datepicker
-          class="valid-item"
-          placeholder="Select Date To"
-          :value="selectDateTo"
-          format="yyyy-MM-dd"
-          @input="selectDateTo = fixDate($event)"
-        ></Datepicker>
-      </div>
-      <div class="submit">
-        <input type="submit" value="Submit" @click="getAllOrders()" />
-      </div>
+        <div class="text-center">
+          <button type="submit" class="button" @click="getAllOrders()">
+            Submit
+          </button>
+        </div>
+      </form>
       <div v-if="orders.length">
         <div class="orders">
           <div class="summary">
-            <div class="summary-num-orders">Number of orders: {{orders.length}}</div>
-            <div
-              class="summary-total-invoice"
-            >The total amount to invoice: {{ totalAmountToInvoice(orders) }} EUR</div>
-            <div class="summary-date-from">Date From: {{selectDateFrom}}</div>
-            <div class="summary-date-to">Select To: {{selectDateTo}}</div>
-            <div
-              class="summary-diference-days"
-            >Diference in Days: {{ diferenceInDays(selectDateFrom,selectDateTo) }} days</div>
+            <div class="summary-num-orders">
+              Number of orders: {{ orders.length }}
+            </div>
+            <div class="summary-total-invoice">
+              The total amount to invoice:
+              {{ totalAmountToInvoice(orders) }} EUR
+            </div>
+            <div class="summary-date-from">Date From: {{ selectDateFrom }}</div>
+            <div class="summary-date-to">Select To: {{ selectDateTo }}</div>
+            <div class="summary-diference-days">
+              Diference in Days:
+              {{ diferenceInDays(selectDateFrom, selectDateTo) }} days
+            </div>
           </div>
           <ul id="orders-list">
-            <li v-for="(order,index) in orders" v-bind:key="index">
-              <div class="order-id">{{order.id}}</div>
-              <div class="order-recipient-name">{{order.recipient.name}}</div>
-              <div class="order-recipient-email">{{order.recipient.email}}</div>
-              <div class="order-total-price">{{ totalPriceOrder(order.items) }} EUR</div>
-              <div class="order_created">{{order.created_at}}</div>Items:
-              <div v-for="(item,index) in order.items" v-bind:key="index" class="order-items">
-                <div class="order-item-id">{{item.id}}</div>
-                <div class="order-item-name">{{item.name}}</div>
-                <div class="order-item-quantity">{{item.quantity}}</div>
-                <div class="order-item-currency">{{item.total_price.currency}}</div>
-                <div class="order-item-amount">{{item.total_price.amount}}</div>
-              </div>Delivery Details:
+            <li v-for="(order, index) in orders" v-bind:key="index">
+              <div class="order-id">{{ order.id }}</div>
+              <div class="order-recipient-name">{{ order.recipient.name }}</div>
+              <div class="order-recipient-email">
+                {{ order.recipient.email }}
+              </div>
+              <div class="order-total-price">
+                {{ totalPriceOrder(order.items) }} EUR
+              </div>
+              <div class="order_created">{{ order.created_at }}</div>
+              Items:
+              <div
+                v-for="(item, index) in order.items"
+                v-bind:key="index"
+                class="order-items"
+              >
+                <div class="order-item-id">{{ item.id }}</div>
+                <div class="order-item-name">{{ item.name }}</div>
+                <div class="order-item-quantity">{{ item.quantity }}</div>
+                <div class="order-item-currency">
+                  {{ item.total_price.currency }}
+                </div>
+                <div class="order-item-amount">
+                  {{ item.total_price.amount }}
+                </div>
+              </div>
+              Delivery Details:
               <div class="order-delivery">
-                <div class="order-delivery-courier">{{order.delivery.courier}}</div>
-                <div class="order-delivery-method">{{order.delivery.method}}</div>
+                <div class="order-delivery-courier">
+                  {{ order.delivery.courier }}
+                </div>
+                <div class="order-delivery-method">
+                  {{ order.delivery.method }}
+                </div>
               </div>
             </li>
           </ul>
         </div>
       </div>
     </div>
-    <div class="invalid" v-else>
-      No data
-    </div>
+    <div class="invalid" v-else>No data</div>
   </div>
 </template>
 
@@ -151,7 +175,15 @@ export default {
 .valid-item {
   margin: 10px auto;
 }
-.submit {
-  background-color: aliceblue;
+.button {
+  background: #3ab409;
+  padding: 0.5rem 1rem;
+  color: white;
+  text-transform: uppercase;
+  font-weight: bold;
+  letter-spacing: 3px;
+  border-radius: 2px;
+  border-bottom: 2px solid #2e9106;
+  transition: 0.3s;
 }
 </style>
